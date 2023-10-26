d744e97cf549bf2883b7c2d54775dcd4c78531dc
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2023-10-26 18:44:59 +0600

    google and github auth added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 const HomePage = () => {
+  const { data: session } = useSession();
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+        Welcome To Next Auth Home Page
+      </h1>
+      {session?.user?.name && (
+        <h3 style={{ textAlign: "center" }}>
+          Logged in user: {session?.user?.name}
+        </h3>
+      )}
     </div>
   );
 };
